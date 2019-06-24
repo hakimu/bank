c8b8e6464bf7955547ec662a09b0f4cda6257802
Hakimu Jackson <hakimu@newrelic.com>
2019-06-23 19:09:21 -0700

    Update User spec

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,7 @@
 FactoryBot.define do
   factory :user do
     username { 'hakimu' }
+    email { 'h@example.com' }
     password { 'password' }
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#create' do
+    context 'when given invalid attributes' do
+      it 'does not create a user without a username' do
+        user = build(:user, username: nil)
+        user.save
+        expect(user.errors.full_messages).to include('Username can\'t be blank')
+      end
+      it 'does not create a user when there\'s an existing username' do
+        create(:user)
+        new_user = build(:user, username: 'hakimu', email: 'hakimu@example.com', password: 'password')
+        new_user.save
+        expect(new_user.errors.full_messages).to include('Username has already been taken')
+      end
+    end
+  end
 end
